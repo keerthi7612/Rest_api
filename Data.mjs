3a267b655d247c568cba0c6fc9f6b4738e1faef2
keerthi7612 <keerthanakumari2003@gmail.com>
2025-11-20 19:21:02 +0530

    Initial commit

---
+++ Data.mjs
@@ -0,0 +1,37 @@
+export const book = [
+  {
+    id: 1,
+    title: "It Ends With Us",
+    description: "emotional romantic drama",
+    price: 350,
+    author: "Colleen Hoover",
+  },
+  {
+    id: 2,
+    title: "The Kite Runner",
+    description: "friendship guilt redemption",
+    price: 450,
+    author: "Khaled Hosseini",
+  },
+  {
+    id: 3,
+    title: "Verity",
+    description: "dark psychological thriller",
+    price: 380,
+    author: "Colleen Hoover",
+  },
+  {
+    id: 4,
+    title: "The Silent Patient",
+    description: "mysterious gripping suspense",
+    price: 420,
+    author: "Alex Michaelides",
+  },
+  {
+    id: 5,
+    title: "Never Lie",
+    description: "twisty chilling mystery",
+    price: 390,
+    author: "Freida McFadden",
+  },
+];
